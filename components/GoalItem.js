a05ade862bd1e15da48044c61a97a3a1e918b51e
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-12-29 18:49:56 -0500

    Using a function on styles

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,7 +3,9 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 const GoalItem = (props) => {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+        <Pressable android_ripple={{backgroundColor: '#cccccc'}} 
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({pressed}) => pressed && styles.pressing}>
             <View style={styles.goalItem}>
                 <Text style={styles.goalItemText}>{props.text}</Text>
             </View>
@@ -16,11 +18,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
       margin: 8,
-      padding: 8,
       borderRadius: 6,
       backgroundColor: "#5e0acc",
     },
     goalItemText: {
       color: 'white',
-    }
+      padding: 8,
+    },
+    pressing: {
+        opacity: 0.4,
+    },
   });
